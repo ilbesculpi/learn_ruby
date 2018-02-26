d4c75973e4d736a06c98da017c02405a39bf6099
ilbert esculpi <ilbert.esculpi@gmail.com>
2018-02-25 20:23:39 -0400

    solutions

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+	# write your code here
+	attr_accessor :title
+
+	def title=(value)
+		conjunctions = ["and", "or", "on", "in", "the", "a", "an", "of"]
+		words = value.split(" ").map { |x|
+			if conjunctions.include?(x)
+				x
+			else
+				x.capitalize
+			end
+		}
+		words[0].capitalize!
+  		@title = words.join(" ")
+	end
+
 end
