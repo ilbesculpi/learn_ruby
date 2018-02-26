4ddca6251495c29cad41d99d3701802537bfc137
ilbert esculpi <ilbert.esculpi@gmail.com>
2018-02-25 20:31:04 -0400

    solutions

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,18 @@
 class Timer
   #write your code here
+
+  	attr_accessor :seconds
+
+	def initialize
+		@seconds = 0
+	end
+
+	def time_string
+		hh = ((@seconds / 3600) % 60).to_s.rjust(2, "0")
+		mm = ((@seconds / 60) % 60).to_s.rjust(2, "0")
+		ss = (@seconds % 60).to_s.rjust(2, "0")
+		arr = [hh, mm, ss]
+		return arr.join(":")
+	end
+
 end
